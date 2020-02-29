5cf2869d00e0f5b7f95a4607842482c7a66b9390
Olli Rousu <olli.rousu@hotmail.com>
2020-02-29 14:04:40 +0200

    Updates

---
+++ precache-manifest.69fef3cef7b3d2ab7dc6c9e689a6a899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4807232873f2d5d66fee72f545fa4876",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "ea7e5b64de37c903adec",
+    "url": "/clicker-game/static/css/main.0c1b00a7.chunk.css"
+  },
+  {
+    "revision": "fd2923f964e71d81e752",
+    "url": "/clicker-game/static/js/2.04614840.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/clicker-game/static/js/2.04614840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7e5b64de37c903adec",
+    "url": "/clicker-game/static/js/main.7906ea2f.chunk.js"
+  },
+  {
+    "revision": "bef762ff6e155ef77d87",
+    "url": "/clicker-game/static/js/runtime-main.88646e64.js"
+  }
+]);
